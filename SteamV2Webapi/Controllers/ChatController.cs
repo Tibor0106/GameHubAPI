b38c5c0252105025af2c4d9d3a3b9c93dc6f8d99
Tibor <112723857+Tibor0106@users.noreply.github.com>
2024-01-29 16:50:14 +0100

    Nehany dolog

---
+++ SteamV2Webapi/Controllers/ChatController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PTHUWEBAPI.Database;
+using SteamV2Webapi.DTO;
+
+namespace SteamV2Webapi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ChatController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+        public ChatController(AppDbContext appDbcontext)
+        {
+            _appDbContext = appDbcontext;
+        }
+
+        //limit messages 1 page = 20 message
+        [HttpGet]
+        [Route("getUserMessages/{id}/{page}")]
+        public async Task<IActionResult> getMessages(int id, int page)
+        {
+
+            return Ok();
+        }
+        [HttpPut]
+        [Route("sendMessage")]
+        public async Task<IActionResult> sendMessage(MessageSendDTO msdto)
+        {
+
+            return Ok();
+        }
+    }
+}
---
+++ SteamV2Webapi/DTO/MessageSendDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace SteamV2Webapi.DTO
+{
+    public class MessageSendDTO
+    {
+        public string Message { get; set; }
+        public int MessageId { get; set; }
+        public int senderId { get; set; }
+        public int reciverId { get; set; }
+
+    }
+}
