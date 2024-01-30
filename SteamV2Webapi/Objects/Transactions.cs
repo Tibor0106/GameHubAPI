dd2d21d3e6c930755aa6cd62fd7bb1611956cc79
MarcellK06 <150803469+MarcellK06@users.noreply.github.com>
2024-01-30 08:46:01 +0100

    Add transactions

---
+++ SteamV2Webapi/Objects/Transactions.cs
@@ -0,0 +1,8 @@
+{
+    public class Transactions
+    {
+        public int transactionId {  get; set; }
+        public Dictionary<string, string> transactionData { get; set; }
+        public int price { get; set; }
+    }
+}
