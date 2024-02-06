8276f8b066bfaf00eacc4415bb0d17f36f0ce210
MarcellK06 <150803469+MarcellK06@users.noreply.github.com>
2024-02-06 19:42:26 +0100

    Shop Controller

---
+++ SteamV2Webapi/Controllers/ShopController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using PTHUWEBAPI.Database;
+using SteamV2Webapi.Objects;
+using SteamV2Webapi.DTO.Shop;
+
+namespace SteamV2Webapi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ShopController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+        public ShopController(AppDbContext appDbcontext)
+        {
+            _appDbContext = appDbcontext;
+        }
+
+        [HttpPut]
+        [Route("AddGame")]
+        public async Task<IActionResult> addGame(ShopDTO sDTO)
+        {
+            _appDbContext.shop.Add(new Shop(0, sDTO.gameId, sDTO.publisherId, sDTO.price, sDTO.discount, sDTO.popularity, sDTO.featured));
+            await _appDbContext.SaveChangesAsync();
+            return Ok(true);
+        }
+        [HttpDelete]
+        [Route("RemoveGame")]
+        public async Task<IActionResult> removeGame(int shopId)
+        {
+            var game = _appDbContext.shop.FirstOrDefault(i => i.Id == shopId);
+            if (game == null)
+                return BadRequest();
+            _appDbContext.shop.Remove(game);
+            await _appDbContext.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
---
+++ SteamV2Webapi/DTO/Shop/ShopDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace SteamV2Webapi.DTO.Shop
+{
+    public class ShopDTO
+    {
+        public int gameId { get; set; }
+        public int publisherId { get; set; }
+        public int price { get; set; }
+        public int discount { get; set; }
+        public int popularity { get; set; }
+        public bool featured { get; set; }
+    }
+}
